01e0d83ab625d00eaf36a93da8d000bde93699f3
icaapriliani <icaaprilianikd.com>
2026-04-26 11:04:08 +0700

    create book model

---
+++ lib/models/book.dart
@@ -0,0 +1,15 @@
+class Book{
+  final String title;
+  final String author;
+  final String rating;
+  final String progress;
+  final String status;
+
+  Book({
+    required this.title,
+    required this.author,
+    required this.rating,
+    required this.progress,
+    required this.status,
+  });
+}
